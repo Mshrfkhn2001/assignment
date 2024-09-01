4617309b01b42389c7738fc1bbf9a9cc2dfb5f3d
Musharraf Khan <100278581+Mshrfkhn2001@users.noreply.github.com>
2024-09-01 12:35:53 +0530

    Update app.js

--- todolist/backend/app.js
+++ todolist/backend/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
-mongoose.connect('mongodb://localhost:27017/todolist').then(()=>{
+mongoose.connect('mongodb+srv://mshrfkhn2001:9415804905@cluster0.zjsd0md.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
   console.log("mongodb is connected");
   
 });
